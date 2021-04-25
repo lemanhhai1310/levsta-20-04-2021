8ecb59494f533dc5b68d31ae8375bdd1e8f2a6e3
Le Manh HAI <lemanhhai@Les-Mac-mini.local>
2021-04-25 17:19:17 +0700

    update

---
+++ batdongsan-detail.php
@@ -0,0 +1,167 @@
+<?php $data["title"] = "Bất động sản chi tiet"; ?>
+<?php $is_home = false; ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-section-small uk-light uk-background-norepeat uk-background-cover uk-background-center-center uk-background-fixed" data-src="images/1x/bg-bds.png" uk-img uk-height-viewport="offset-top: true;offset-bottom: true;">
+    <div class="uk-container">
+        <div class="" uk-grid>
+            <div class="uk-width-1-4@m">
+
+            </div>
+            <div class="uk-width-expand">
+                <div id="thongsoduan" class="uk-margin-large">
+                    <div class="mb-15 mb-20-m">
+                        <h3 class="uk-h3 uk-margin-remove bds__contentRight__title">THÔNG SỐ DỰ ÁN</h3>
+                    </div>
+                    <div class="uk-cover-container mb-20 mb-30-m">
+                        <img src="images/1x/Asset2.png" alt="" uk-cover>
+                        <canvas width="1384" height="596"></canvas>
+                    </div>
+                    <div uk-grid>
+                        <div class="uk-width-1-2@m">
+                            <ul class="uk-list bds__contentRight__list1">
+                                <li><span>Chủ đầu tư:</span> Tập đoàn Country Garden</li>
+                                <li><span>Đối tác phối hợp thực hiện:</span> EDSB, Cisco, Huawei, SSAKI, Arup</li>
+                                <li><span>Tổng diện tích đất:</span> 20km2</li>
+                                <li><span>Sản phẩm:</span> Căn hộ, Condotel, biệt thự, khách sạn và hệ thống tiện ích</li>
+                                <li><span>Quy mô:</span> 4 đảo nhân tạo</li>
+                                <li><span>Vị trí:</span>  Johor Bahru, Malaysia</li>
+                                <li><span>Tổng vốn đầu tư:</span> 100 tỷ USD</li>
+                                <li><span>Thời gian khởi công:</span> năm 2013</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="thongtin" class="uk-margin-large">
+                    <div class="uk-margin-medium">
+                        <div class="mb-10 mb-15-m">
+                            <h3 class="uk-h3 uk-margin-remove bds__contentRight__title">SỞ HỮU NHÀ Ở VĨNH VIỄN</h3>
+                        </div>
+                        <div uk-grid>
+                            <div class="uk-width-1-2@m">
+                                <div class="bds__contentRight__box1">
+                                    Đầu tư từ 532.000RM (tương ứng khoảng 3 tỷ VNĐ) với khoảng hơn 53.000 RM (khoảng 300 triệu VNĐ) thanh toán đợt đầu tiên có thể sở hữu căn hộ chung cư cao cấp diện tích 48m2, 2 phòng ngủ tại Forest City.
+                                    Với mức giá bán hợp lý, Forest City Malaysia là lựa chọn của nhiều người đang làm việc tại Singapore để có thể vừa sở hữu nhà ở lâu dài, vừa thuận tiện cho công việc và hoạt động nghỉ dưỡng, giải trí.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-margin-medium">
+                        <div class="mb-10 mb-15-m">
+                            <h3 class="uk-h3 uk-margin-remove bds__contentRight__title">TIỀM NĂNG TĂNG GIÁ LỚN</h3>
+                        </div>
+                        <div uk-grid>
+                            <div class="uk-width-1-2@m">
+                                <div class="bds__contentRight__box1">
+                                    Một trong những yếu tố hấp dẫn dòng tiền đầu tư chính là tiềm năng tăng giá trong tương lai. Mỗi nhà đầu tư có một sự tính toán mức tiền kỳ vọng và mức tính toán đó dựa trên một cơ sở chắc chắn. Hiện tại mức giá căn hộ tại dự án Forest City Malaysia khá hấp dẫn chỉ bằng 1/7 tới 1/4 giá trị bất động sản tại các thủ đô Singapore, Vancouver, NewYork và Sydney.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-margin-medium">
+                        <div class="mb-10 mb-15-m">
+                            <h3 class="uk-h3 uk-margin-remove bds__contentRight__title">MÔI TRƯỜNG SỐNG XANH - AN TOÀN - VĂN MINH</h3>
+                        </div>
+                        <div uk-grid>
+                            <div class="uk-width-1-2@m">
+                                <div class="bds__contentRight__box1">
+                                    Forest City Malaysia được xây dựng theo mô hình đô thị lập thể: trên mặt đất là nhà ở, văn phòng, hệ thống giao thông ngầm dưới mặt đất (xe cộ, tàu điện). Mọi hoạt động của thành phố được quản lý bởi hệ thống công nghệ tân tiến, tự động – là SMART CITY kiểu mẫu của đô thị trong tương lai. Không chỉ lớp cư dân hiện tại mà các thế hệ tương lai sẽ hưởng lợi từ quy hoạch chuẩn mực.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="thutucdinhcu" class="uk-margin-large">
+                    <div class="uk-margin-medium">
+                        <div class="mb-10 mb-15-m">
+                            <h3 class="uk-h3 uk-margin-remove bds__contentRight__title">THỦ TỤC ĐỊNH CƯ VÀ TÍNH PHÁP LÝ CỦA DỰ ÁN  </h3>
+                        </div>
+                        <div uk-grid>
+                            <div class="uk-width-1-2@m">
+                                <div class="bds__contentRight__box1">
+                                    Đầu tư từ 532.000RM (tương ứng khoảng 3 tỷ VNĐ) với khoảng hơn 53.000 RM (khoảng 300 triệu VNĐ) thanh toán đợt đầu tiên có thể sở hữu căn hộ chung cư cao cấp diện tích 48m2, 2 phòng ngủ tại Forest City.
+                                    Với mức giá bán hợp lý, Forest City Malaysia là lựa chọn của nhiều người đang làm việc tại Singapore để có thể vừa sở hữu nhà ở lâu dài, vừa thuận tiện cho công việc và hoạt động nghỉ dưỡng, giải trí.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-margin-medium">
+                        <div class="uk-cover-container">
+                            <img src="images/1x/Asset3.png" alt="" uk-cover>
+                            <canvas width="1384" height="446"></canvas>
+                        </div>
+                    </div>
+                    <div class="uk-margin-medium">
+                        <div uk-grid>
+                            <div class="uk-width-1-2@m">
+                                <div class="mb-10 mb-15-m">
+                                    <h4 class="uk-h4 uk-margin-remove bds__contentRight__title1">Về định cư:</h4>
+                                </div>
+                                <div class="bds__contentRight__box1">
+                                    Chính phủ Malaysia áp dụng chương trình “Ngôi nhà thứ 2” tại Forest City. Theo đó, khi sở hữu căn hộ tại đây, chủ nhân căn hộ và gia đình (cha mẹ, và các con dưới 21 tuổi) có cơ hội được cấp visa dài hạn trong 10 năm. Được hưởng các chính sách ưu đãi  và miễn phí về giáo dục y tế tương tự công dân Malaysia.
+                                    Không yêu cầu tiếng Anh, không cần thay đổi quốc tịch.
+                                </div>
+                            </div>
+                            <div class="uk-width-1-2@m">
+                                <div class="mb-10 mb-15-m">
+                                    <h4 class="uk-h4 uk-margin-remove bds__contentRight__title1">Về sở hữu bất động sản:</h4>
+                                </div>
+                                <div class="bds__contentRight__box1">
+                                    Dự án có quyền sử dụng đất vĩnh viễn, quyền sở hữu có thể được chuyển giao cho thế hệ kế tiếp (không mất thuế thừa kế)
+                                    Không phải đóng bất cứ loại thuế nhà đất nào, chỉ đóng phí quản lý
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="phuongthucthanhtoan" class="uk-margin-large">
+                    <div class="uk-margin-medium">
+                        <div class="mb-10 mb-15-m">
+                            <h3 class="uk-h3 uk-margin-remove bds__contentRight__title">PHƯƠNG THỨC THANH TOÁN</h3>
+                        </div>
+                        <div uk-grid>
+                            <div class="uk-width-1-2@m">
+                                <div class="mb-10 mb-15-m">
+                                    <h4 class="uk-h4 uk-margin-remove bds__contentRight__title2">Giai đoạn đăng ký mua:</h4>
+                                </div>
+                                <div class="bds__contentRight__box1">
+                                    <div>Giữ chỗ và ký HĐ trong ngày 7 ngày</div>
+                                    <div>Giữ chỗ và ký HĐ trong vòng 15 ngày</div>
+                                    <div>Giữ chỗ và ký HĐ sau vòng 15 ngày</div>
+                                </div>
+                            </div>
+                            <div class="uk-width-1-2@m">
+                                <div class="mb-10 mb-15-m">
+                                    <h4 class="uk-h4 uk-margin-remove bds__contentRight__title2">Giai đoạn ký hợp đồng mua bán:</h4>
+                                </div>
+                                <div class="bds__contentRight__box1">
+                                    <div>Đặt cọc 10%, 90% thanh toán theo lịch (Tối đa lên đến 4 năm)</div>
+                                    <div>Phương thức 1: Thanh toán 90% trong vòng 2 tháng</div>
+                                    <div>Phương thức 2: Thanh toán 90% trong vòng 6 tháng</div>
+                                    <div>Phương thức 3: Thanh toán trong 1 năm</div>
+                                    <div>Phương thức 4: Thanh toán trong 2 năm</div>
+                                    <div>Phương thức 5: Thanh toán trong 3 năm</div>
+                                    <div>Phương thức 6: Thanh toán từ 1 năm trỏ lên, mỗi 6 tháng thanh toán 1 lần, thanh toán đủ mới được nhận nhà.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-margin-medium">
+                        <div class="uk-cover-container">
+                            <img src="images/1x/Asset4.png" alt="" uk-cover>
+                            <canvas width="1582" height="658"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div id="tienich" class="uk-margin-large">
+                    <div class="uk-margin-medium">
+                        <div class="mb-15 mb-20-m">
+                            <h3 class="uk-h3 uk-margin-remove bds__contentRight__title">TIỆN ÍCH ĐẲNG CẤP TẠI DỰ ÁN</h3>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
