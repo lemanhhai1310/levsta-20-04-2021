01bbacc4b0e4054f3ec33638cdeafb679d2e9dea
Le Manh HAI <lemanhhai@Les-Mac-mini.local>
2021-05-05 10:51:16 +0700

    update

--- batdongsan.php
+++ batdongsan.php
@@ -24,44 +24,72 @@
         </div>
     </div>
 </div>
-<div class="home__block01 uk-grid-match uk-overflow-hidden uk-grid-collapse uk-child-width-1-2 uk-child-width-1-4@m" uk-height-viewport="offset-top: true" uk-grid uk-scrollspy="cls: uk-animation-slide-bottom; target: .animation; delay: 300; repeat: false">
-    <?php
-    $data = array(
-        array(
-            'src' => 'images/1x/bg1.png',
-            'txt' => 'THE ROYAL
-DANANG BOUTIQUE HOTEL & CONDO',
-        ),
-        array(
-            'src' => 'images/chung-cu-haven-park-2.jpg',
-            'txt' => 'Risemount Apartment Da Nang
-Tokyo Bên Dòng Sông Hàn',
-        ),
-        array(
-            'src' => 'images/1x/bg2.png',
-            'txt' => 'THE METROPOLE THU THIEM
-THE OPERA RESIDENCES',
-        ),
-        array(
-            'src' => 'images/1x/bg3.png',
-            'txt' => 'THE METROPOLE THU THIEM
-THE OPERA RESIDENCES',
-        ),
-    );
-    foreach ($data as $k => $v): ?>
-        <div>
-            <div uk-scrollspy-class="<?= ($k%2 == 1) ? 'uk-animation-slide-top' : '' ?>" class="uk-inline-clip uk-transition-toggle animation" tabindex="0">
-                <div class="uk-transition-scale-up uk-height-1-1 uk-transition-opaque uk-background-norepeat uk-background-cover uk-background-center-center" data-src="<?= $v['src'] ?>" uk-img>
+<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
 
-                </div>
-                <a href="#" class="uk-link-toggle uk-position-cover home__block01__bg1 uk-padding-small uk-flex uk-flex-middle uk-flex-center">
-                    <div class="uk-text-center" uk-toggle="cls: home__block01__marginTop; mode: media; media: @m">
-                        <h2 class="uk-h2 home__block01__title1"><?= $v['txt'] ?></h2>
+    <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
+        <?php
+        $data = array(
+            array(
+                'src' => 'images/1x/bg1.png',
+                'txt' => 'THE ROYAL
+DANANG BOUTIQUE HOTEL & CONDO',
+            ),
+            array(
+                'src' => 'images/chung-cu-haven-park-2.jpg',
+                'txt' => 'Risemount Apartment Da Nang
+Tokyo Bên Dòng Sông Hàn',
+            ),
+            array(
+                'src' => 'images/1x/bg2.png',
+                'txt' => 'THE METROPOLE THU THIEM
+THE OPERA RESIDENCES',
+            ),
+            array(
+                'src' => 'images/1x/bg3.png',
+                'txt' => 'THE METROPOLE THU THIEM
+THE OPERA RESIDENCES',
+            ),
+            array(
+                'src' => 'images/1x/bg1.png',
+                'txt' => 'THE ROYAL
+DANANG BOUTIQUE HOTEL & CONDO',
+            ),
+            array(
+                'src' => 'images/chung-cu-haven-park-2.jpg',
+                'txt' => 'Risemount Apartment Da Nang
+Tokyo Bên Dòng Sông Hàn',
+            ),
+            array(
+                'src' => 'images/1x/bg2.png',
+                'txt' => 'THE METROPOLE THU THIEM
+THE OPERA RESIDENCES',
+            ),
+            array(
+                'src' => 'images/1x/bg3.png',
+                'txt' => 'THE METROPOLE THU THIEM
+THE OPERA RESIDENCES',
+            ),
+        );
+        foreach ($data as $k => $v): ?>
+            <li>
+                <div uk-scrollspy-class="<?= ($k%2 == 1) ? 'uk-animation-slide-top' : '' ?>" class="uk-inline-clip uk-transition-toggle animation" tabindex="0">
+                    <div class="uk-transition-scale-up uk-transition-opaque uk-cover-container">
+                        <img src="<?= $v['src'] ?>" alt="" uk-cover>
+                        <canvas width="469" height="975"></canvas>
                     </div>
-                </a>
-            </div>
-        </div>
-    <?php endforeach; ?>
+                    <a href="#" class="uk-link-toggle uk-position-cover home__block01__bg1 uk-padding-small uk-flex uk-flex-middle uk-flex-center">
+                        <div class="uk-text-center" uk-toggle="cls: home__block01__marginTop; mode: media; media: @m">
+                            <h2 class="uk-h2 home__block01__title1"><?= $v['txt'] ?></h2>
+                        </div>
+                    </a>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
 </div>
 <ul class="uk-pagination uk-flex-center" uk-margin>
     <li><a href="#"><span uk-pagination-previous></span></a></li>
